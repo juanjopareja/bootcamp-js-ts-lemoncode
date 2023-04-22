3ce6d8d5485479c34c5c8ccd05074ae5c8234810
Juanjo Pareja <125128610+juanjopareja@users.noreply.github.com>
2023-04-22 17:01:25 +0200

    Add files via upload

---
+++ Módulo 02 - Tipos de datos y Operadores/src/main.ts
@@ -0,0 +1,8 @@
+import "./style.css";
+
+console.log("Precio total del ticket: " + 120 + "€");
+console.log("Precio de las bebidas: " + 18 + "€");
+console.log("Cantidad a pagar entre todos: " + (120 - 18) + "€");
+console.log("Número total de personas: " + 6);
+console.log("Cantidad a pagar por persona (102€ / 6): " + 102 / 6 + "€");
+
