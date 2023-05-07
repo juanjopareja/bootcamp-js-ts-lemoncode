ec68fe0cbeab5820d4d1d288ef751e7e019f9acd
Juanjo Pareja <125128610+juanjopareja@users.noreply.github.com>
2023-05-07 19:51:11 +0200

    Add files via upload

---
+++ Módulo 04 - Funciones/src/main.ts
@@ -0,0 +1,50 @@
+import "./style.css";
+
+const turnNumber = document.getElementById("turn-number");
+
+// Functions
+function resetTurnNumber() {
+    setNumber(0);
+}
+
+function setNumber(number : number) {
+    if (turnNumber !== null && turnNumber !== undefined) {
+        turnNumber.innerHTML = `${number}`.padStart(2,'0');
+    }
+}
+
+function followNumber() {
+    if (turnNumber) {
+        let newNumber = Number(turnNumber.innerHTML);
+
+        if (newNumber !== null && newNumber !== undefined && newNumber < 9999) {
+            newNumber++;
+            setNumber(newNumber);
+        }
+    }
+}
+
+function previousNumber() {
+    if (turnNumber) {
+        let newNumber = Number(turnNumber.innerHTML);
+
+        if (newNumber !== null && newNumber !== undefined && newNumber > 0) {
+            newNumber--;
+            setNumber(newNumber);
+        }
+    }
+}
+
+function personalTurn() {
+    const personalTurn = document.getElementById("personal-turn-number"); 
+    if (personalTurn && personalTurn instanceof HTMLInputElement) {
+        setNumber(Number(personalTurn.value));        
+    }
+}
+
+
+// Events
+document.getElementById("reset")?.addEventListener("click", resetTurnNumber);
+document.getElementById("follow-button")?.addEventListener("click", followNumber);
+document.getElementById("previous-button")?.addEventListener("click", previousNumber);
+document.getElementById("insert-number")?.addEventListener("click", personalTurn);
