4a4d2e942dbe26f6400e3eb139dd08cb7aff7b21
juanjopareja <jupg25@msn.com>
2024-03-10 12:48:08 +0100

    Add main.ts upload

--- Módulo 08 - Arrays y Bucles/src/main.ts
+++ Módulo 08 - Arrays y Bucles/src/main.ts
@@ -1,3 +1,77 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+type Especialidad = "Médico de familia" | "Pediatra" | "Cardiólogo";
+
+interface Pacientes {
+    id: number;
+    nombre: string;
+    apellidos: string;
+    sexo: string;
+    temperatura: number;
+    frecuenciaCardiaca: number;
+    especialidad: Especialidad;
+    edad: number;
+}
+
+const pacientes: Pacientes[] = [
+    {
+        id: 1,
+        nombre: "John",
+        apellidos: "Doe",
+        sexo: "Male",
+        temperatura: 36.8,
+        frecuenciaCardiaca: 80,
+        especialidad: "Médico de familia",
+        edad: 44,
+    },
+    {
+        id: 2,
+        nombre: "Jane",
+        apellidos: "Doe",
+        sexo: "Female",
+        temperatura: 36.8,
+        frecuenciaCardiaca: 70,
+        especialidad: "Médico de familia",
+        edad: 43,
+    },
+    {
+        id: 3,
+        nombre: "Junior",
+        apellidos: "Doe",
+        sexo: "Male",
+        temperatura: 36.8,
+        frecuenciaCardiaca: 90,
+        especialidad: "Pediatra",
+        edad: 8,
+    },
+    {
+        id: 4,
+        nombre: "Mary",
+        apellidos: "Wien",
+        sexo: "Female",
+        temperatura: 36.8,
+        frecuenciaCardiaca: 120,
+        especialidad: "Médico de familia",
+        edad: 20,
+    },
+    {
+        id: 5,
+        nombre: "Scarlett",
+        apellidos: "Somez",
+        sexo: "Female",
+        temperatura: 36.8,
+        frecuenciaCardiaca: 110,
+        especialidad: "Cardiólogo",
+        edad: 30,
+    },
+    {
+        id: 6,
+        nombre: "Brian",
+        apellidos: "Kid",
+        sexo: "Male",
+        temperatura: 39.8,
+        frecuenciaCardiaca: 80,
+        especialidad: "Pediatra",
+        edad: 11,
+    },
+];
