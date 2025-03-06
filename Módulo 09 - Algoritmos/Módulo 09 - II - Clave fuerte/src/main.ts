afa447df11b07e33d5b7e38b2bd6131cc5c3b8f1
juanjopareja <jupg25@msn.com>
2025-03-06 15:06:29 +0100

    Add files update

--- Módulo 09 - Algoritmos/Módulo 09 - II - Clave fuerte/src/main.ts
+++ Módulo 09 - Algoritmos/Módulo 09 - II - Clave fuerte/src/main.ts
@@ -1,3 +1,28 @@
 import "./style.css";
+import { validarClave } from "./motor";
+import { commonPasswords } from "./constantes";
+
+
+const mostrarValidacion = (nombreUsuario: string, clave: string): void => {
+    const resultado = validarClave(nombreUsuario, clave, commonPasswords);
+
+    const resultadoValidacion = {
+        nombreUsuario: nombreUsuario,
+        clave: clave,
+        mensaje: resultado.error
+    }
+
+    console.log("Nombre de usuario: " + resultadoValidacion.nombreUsuario);
+    console.log("Clave: " + resultadoValidacion.clave);
+    console.log("Mensaje: " + resultadoValidacion.mensaje);
+}
+
+const nombreIntroducido = "juanjo";
+const claveIntroducida = "juanjo";
+
+document.addEventListener("DOMContentLoaded", () => {
+    mostrarValidacion(nombreIntroducido, claveIntroducida);
+});
+
 
 
--- Módulo 09 - Algoritmos/Módulo 09 - II - Clave fuerte/src/motor.ts
+++ Módulo 09 - Algoritmos/Módulo 09 - II - Clave fuerte/src/motor.ts
@@ -1,33 +1,90 @@
 import { ValidacionClave } from "./modelo";
 
-const validarClave = (
-    nombreUsuario: string,
-    clave: string,
-    commonPasswords: string[]
-): ValidacionClave => {
-    // ...
-};
+const mensajeDeError = (error: string): string => {
+    switch (error) {
+        case "errorMayusculasMinusculas":
+            return "La clave debe de tener mayúsculas y minúsculas";
+        
+        case "errorNumeros":
+            return "La clave debe de tener números";
+        
+        case "errorCaracterEspecial":
+            return "La clave debe de tener caracteres especiales";
+        
+        case "errorLongitud":
+            return "La clave debe de tener una longitud mínima de 8 caracteres";
+        
+        case "errorNombreUsuario":
+            return "La clave no debe contener el nombre del usuario";
+        
+        case "errorPalabrasComunes":
+            return "La clave no debe de contener palabras comunes";
+        
+        default:
+            return "Formato de clave válido";
+    }
+}
 
 const tieneMayusculasYMinusculas = (clave: string): ValidacionClave => {
-    // ...
+    const tieneMayusculas = /[A-Z]/.test(clave);
+    const tieneMinusculas = /[a-z]/.test(clave);
+
+    return (!tieneMayusculas || !tieneMinusculas) ? 
+        { esValida: false, error: mensajeDeError("errorMayusculasMinusculas") } :
+        { esValida: true, error: "" };
 };
 
 const tieneNumeros = (clave: string): ValidacionClave => {
-    // ...
+    const tieneNumeros = /[0-9]/.test(clave);
+
+    return !tieneNumeros ? 
+        { esValida: false, error: mensajeDeError("errorNumeros") } :
+        { esValida: true, error: "" };
 };
 
 const tieneCaracteresEspeciales = (clave: string): ValidacionClave => {
-    // ...
+    const tieneCaracteresEspeciales = /[!¡@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?¿]+/.test(clave);
+
+    return !tieneCaracteresEspeciales ?
+        { esValida: false, error: mensajeDeError("errorCaracterEspecial") } :
+        { esValida: true, error: "" };
 };
 
 const tieneLongitudMinima = (clave: string): ValidacionClave => {
-    // ...
+    const longitudMinima = clave.length >= 8;
+
+    return !longitudMinima ?
+        { esValida: false, error: mensajeDeError("errorLongitud") } :
+        { esValida: true, error: "" };
 };
 
 const tieneNombreUsuario = (nombreUsuario: string, clave: string): ValidacionClave => {
-   // ...
+    const tieneNombreUsuario = clave.includes(nombreUsuario);
+
+    return tieneNombreUsuario ?
+        { esValida: false, error: mensajeDeError("errorNombreUsuario") } :
+        { esValida: true, error: "" };
 };
 
 const tienePalabrasComunes = (clave: string, commonPasswords: string[]): ValidacionClave => {
-    // ...
+    const tienePalabrasComunes = commonPasswords.includes(clave);
+
+    return tienePalabrasComunes ?
+        { esValida: false, error: mensajeDeError("errorPalabrasComunes") } :
+        { esValida: true, error: "" };
+};
+
+export const validarClave = (nombreUsuario: string, clave: string, commonPasswords: string[]): ValidacionClave => {
+    const validaciones = [
+        tieneNombreUsuario(nombreUsuario, clave),
+        tienePalabrasComunes(clave, commonPasswords),
+        tieneMayusculasYMinusculas(clave),
+        tieneNumeros(clave),
+        tieneCaracteresEspeciales(clave),
+        tieneLongitudMinima(clave)
+    ];
+
+    const validacionIncorrecta = validaciones.find(validacion => !validacion.esValida);
+
+    return validacionIncorrecta ? validacionIncorrecta : { esValida: true, error: mensajeDeError("") };
 };
